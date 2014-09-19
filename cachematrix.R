eede2b51f02b95c01930060bd4550983486e659a
unknown <pnair@BAN4PNAIR-L1.vcorp.ad.vrsn.com>
2014-09-19 16:06:45 +0530

    #Functions for creating a Matrix and caching it

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This function retruns a list with setters and getters.
 makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
 
+## sets the matrix to super variable x. 
+ 	set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+
+        get <- function() x
+        setInverse <- function(inverse) i <<- inverse
+        getInverse <- function() i
+
+## retrun the list with all the functions.
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
+## This function returns a inverse Matric created by the 
+## function makeCacheMatrix. If the inverse is already in the 
+## cache (global env) it will retrun the same from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      
+	 i <- x$getInverse()
+## check if cached
+	 if(!is.null(i)) {
+		 message("getting cached data")
+## retrun from the cache
+		 return(i)
+	 }
+	 data <- x$get()
+## if not cached reverse the matrix.
+	 i <- solve(data, ...)
+	 x$setInverse(i)
+## return the inverse of the matrix.
+	 i
+        
 }
